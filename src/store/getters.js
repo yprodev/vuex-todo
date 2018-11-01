bb5758327c05374bd30e106ffaf33521431d510f
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2018-11-01 14:59:43 +0200

    Working on mistakes: Merged actions, getters, mutations and state into one file.

--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getAllTodos = state => state.todoItems;
-const getCompletedTodos = state => state.todoItems.map(todo => (todo.completed === true));
-const isEmptyTodos = state => state.todoItems.length !== 0;
-
-export default {
-  getAllTodos,
-  getCompletedTodos,
-  isEmptyTodos
-}
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,54 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import state from './state';
-import mutations from './mutations';
-import getters from './getters';
-import actions from './actions';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
-  state,
-  mutations,
-  actions,
-  getters
+  state: {
+    todoItems: [],
+  },
+  mutations: {
+    FETCH_TODOS: (state, todos) => {
+      state.todoItems.push(...todos);
+    },
+    ADD_TODO: (state, todo) => {
+      state.todoItems.push(todo);
+    },
+    UPDATE_STATUS: (state, task) => {
+      const todoIndex = state.todoItems.findIndex(item => item.id === task.id);
+      const foundTodo = state.todoItems.find(item => item.id === task.id);
+      foundTodo.completed = !foundTodo.completed;
+      state.todoItems.splice(todoIndex, 1, foundTodo);
+    }
+  },
+  actions: {
+    fetchTodos: context => {
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve([
+            { id: 1, name: 'Buy bread', completed: false },
+            { id: 2, name: 'Go to meet with the friends', completed: true },
+            { id: 3, name: 'Make some trainings', completed: true },
+            { id: 4, name: 'Create new pet project', completed: false },
+            { id: 5, name: 'Go to bed', completed: false }
+            ]);
+        }, 4000);
+      })
+      .then(data => {
+        context.commit('FETCH_TODOS', data);
+      });
+    }
+  },
+  getters: {
+    getAllTodos: state => {
+      return state.todoItems;
+    },
+    getCompletedTodos: state => {
+      return state.todoItems.filter(todo => todo.completed);
+    },
+    isEmptyTodos: state => {
+      return state.todoItems.length !== 0;
+    }
+  }
 });
