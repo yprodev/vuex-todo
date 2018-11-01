3ec22003eb5543349ae29cd167e844a8f092cd6d
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2018-11-01 16:04:42 +0200

    Working on mistakes: All the styles should be with the scoped attribute.

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 body {
   padding-top: 40px;
 }
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -40,7 +40,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .todo-input {
     margin-bottom: 40px;
   }
